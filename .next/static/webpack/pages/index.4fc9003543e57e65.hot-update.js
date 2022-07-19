15437015b311fa7e6ff6839fac1a01039614cd7b
Thallys <thallysspeed4@gmail.com>
2022-07-19 13:23:49 -0300

    first commit

---
+++ .next/static/webpack/pages/index.4fc9003543e57e65.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./config.json":
+/*!*********************!*\
+  !*** ./config.json ***!
+  \*********************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"name":"Discord - Alura Matrix","theme":{"colors":{"primary":{"100":"#C1EAC5","200":"#A3D9A5","300":"#7BC47F","400":"#57AE5B","500":"red","600":"#2F8132","700":"#207227","800":"#0E5814","900":"#05400A","050":"#E3F9E5"},"neutrals":{"100":"#e4e7eb","200":"#cbd2d9","300":"#9aa5b1","400":"#52667a","500":"#313d49","600":"#29333d","700":"#212931","800":"#181f25","900":"#101418","999":"#080a0c","000":"#FFFFFF","050":"#f5f7fa"}}}}');
+
+/***/ })
+
+});
